e2db5866c04563668adcd97cdd7e328d80939284
kshitiz79 <kshitiz.21b0131170@abes.ac.in>
2024-11-21 13:25:55 +0530

    Your commit message

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,9 @@ app.use(cors({
     credentials: true,
 }));
 app.use(express.json({ limit: "25mb" }));
+app.use(bodyParser.json({ limit: "25mb" })); // Body parser for JSON
+app.use(bodyParser.urlencoded({ limit: "25mb", extended: true })); // Body parser for URL-encoded data
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
